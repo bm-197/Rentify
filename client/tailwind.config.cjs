6fe5960ded620f744c833444d07df68510bb74f2
bm-197 <bisratmaru111@gmail.com>
2024-08-15 01:47:48 +0300

    :wrench: init: intialized server and client project

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        backgroundImage: {
+          "red-car": "url('./car.jpg')",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
