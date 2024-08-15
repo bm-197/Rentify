8e4eede95dc72df82d2dafd854ea9a80984a7514
bm-197 <bisratmaru111@gmail.com>
2024-08-15 11:02:18 +0300

    :star: feat: Add Car model to the database
    - Implement Car model to represent car-related data.
    - Define necessary fields and relationships for the Car entity.

---
+++ server/models/CarModel.js
@@ -0,0 +1,17 @@
+import mongoose from "../utils/db.js";
+
+const carSchema = new mongoose.Schema({
+  carPhoto: String,
+  mark: String,
+  price: Number,
+  ac: String,
+  model: String,
+  door: String,
+  transmission: String,
+  fuel: String,
+  year: String,
+  active: { type: Boolean, default: true },
+  status: { type: String, default: "available" },
+});
+
+export const Car = mongoose.model("car", carSchema);
