569a4fed97c86d640db43b595c37ca7f8e73f45b
bm-197 <bisratmaru111@gmail.com>
2024-08-25 11:56:23 +0300

    :star: feat: Add Choose Us section to Home Page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import PlanTrip from "../components/PlanTrip";
+import ChooseUs from "../components/ChooseUs";
 
 function Home() {
   return (
     <>
       <Hero />
       <PlanTrip />
+      <ChooseUs />
     </>
   );
 }
