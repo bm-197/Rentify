d1e7ff797eac91a2fe46d3181dc8ae48abc64a76
bm-197 <bisratmaru111@gmail.com>
2024-08-26 15:49:24 +0300

    :up: update: modify cord and json parser

--- server/app.js
+++ server/app.js
@@ -17,12 +17,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(cors({ origin: "http://localhost:4000" }));
-app.use(express.json());
+app.use(cors({
+  origin: "*",
+  credentials: true,
+  allowedHeaders: ['Authorization', 'Content-Type'],
+}));
+app.use(express.json({ limit: '10mb' }));
 app.use("/user", userRouter);
-app.get("/", (req, res) => {
-  res.status(200).json({"running": "on port 5000"})
-})
+
+
 app.listen(port, host, (err) => {
   if (err) {
     console.log("server disconnected", err);
