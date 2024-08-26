1251366709ecb1522f573db8387e52f0494f3fd9
bm-197 <bisratmaru111@gmail.com>
2024-08-26 15:36:14 +0300

    :up: update(CarModel): add required element to fields

--- server/models/CarModel.js
+++ server/models/CarModel.js
@@ -1,17 +1,50 @@
-import mongoose from "../utils/db.js";
+import mongoose from '../utils/db.js'
 
 const carSchema = new mongoose.Schema({
-  carPhoto: String,
-  mark: String,
-  price: Number,
-  ac: String,
-  model: String,
-  door: String,
-  transmission: String,
-  fuel: String,
-  year: String,
-  active: { type: Boolean, default: true },
-  status: { type: String, default: "available" },
-});
+  carPhoto: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  mark: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  ac: {
+    type: Boolean,
+    required: true,
+  },
+  door: {
+    type: Number,
+    required: true,
+  },
+  transmission: {
+    type: String,
+    required: true,
+  },
+  fuel: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  status: {
+    type: String,
+    default: "Available",
+  }
+}, { timestamps: true });
+
 
 export const Car = mongoose.model("cars", carSchema);
